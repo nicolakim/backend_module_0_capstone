2797b3be51c5b2ef477d1f0c86ece04c7a66863c
Nicola Kim <nicola.copsey@gmail.com>
2020-02-21 11:49:31 -0800

    Updates to Day 3 Work, convert 2 files to .rb

---
+++ day_3/ex30_study_drills.rb
@@ -0,0 +1,14 @@
+# Study Drills
+#
+# 1. Try to guess what `elsif` and `else` are doing.
+# `elsif` is an alternative with corresponding result, to `if`.
+# `else` is saying "in all cases when neither `if` or `elsif` works, do this", with it's own result.
+
+# 2. Change the numbers of `cars`, `people`, and `trucks` and then trace through each `if-statement` to see what will be printed.
+# yep (updated in ex30rb)
+
+# 3. Try some more complex boolean expressions like `cars > people || trucks < cars`.
+# yep (updated in ex30rb)
+
+# 4. Above each line write an English description of what the line does.
+# yep (see ex30rb)
